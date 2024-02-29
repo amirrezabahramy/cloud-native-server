5833bdceb98f59961866c138ea07cfa4884e6aa7
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-29 13:02:37 +0330

    Added user model

---
+++ src/models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const schema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "customer"],
+      immutable: true,
+    },
+  },
+  { timestamps: { createdAt: "createdAt", updatedAt: "updatedAt" } }
+);
+
+schema.pre("findOneAndUpdate", function (next) {
+  this.setOptions({ runValidators: true, new: true });
+  next();
+});
+
+const model = mongoose.model("User", schema);
+
+module.exports = model;
