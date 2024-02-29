aece87228a63168ce758219b5f3179e17f858e5e
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-29 13:42:39 +0330

    Moved app.get up

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const mongoose = require("mongoose");
 
 const app = express();
 
+app.get("/", function (req, res) {
+  res.send(
+    `App is running on port: ${APP_PORT} and hostname: ${APP_HOSTNAME}.`
+  );
+});
+
 // Core middlewares
 app.use(express.json());
 app.use(cors());
 
 // Router
 app.use("/api/v1", require("./src/routes"));
-
-app.get("/", function (req, res) {
-  res.send(
-    `App is running on port: ${APP_PORT} and hostname: ${APP_HOSTNAME}.`
-  );
-});
 
 dotenv.config();
 // Db start
